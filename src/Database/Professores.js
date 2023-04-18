8835db41344d0d65138b39ec8492f4ecd1c1378c
Thomas <thomasdomingos@hotmail.com>
2023-04-18 01:44:53 -0300

    exercicios

---
+++ src/Database/Professores.js
@@ -0,0 +1,62 @@
+export const ProfessoresData = [
+  {
+    id: 1,
+    nome: "Thomas",
+    nascimento: new Date("01/10/1999"),
+    materia: "Portuges",
+  },
+  {
+    id: 2,
+    nome: "Heitor",
+    nascimento: new Date("09/18/2000"),
+    materia: "Matematica",
+  },
+  {
+    id: 3,
+    nome: "Mike",
+    nascimento: new Date("05/05/2000"),
+    materia: "Portuges",
+  },
+  {
+    id: 4,
+    nome: "Pedro",
+    nascimento: new Date("05/07/1998"),
+    materia: "Matematica",
+  },
+  {
+    id: 5,
+    nome: "Caio",
+    nascimento: new Date("05/07/1997"),
+    materia: "Geografia",
+  },
+  {
+    id: 6,
+    nome: "Caio",
+    nascimento: new Date("05/07/2005"),
+    materia: "Geografia",
+  },
+  {
+    id: 7,
+    nome: "Felipe",
+    nascimento: new Date("05/05/2010"),
+    materia: "Portuges",
+  },
+  {
+    id: 8,
+    nome: "Thiago",
+    nascimento: new Date("05/10/2013"),
+    materia: "Matematica",
+  },
+  {
+    id: 9,
+    nome: "Thiago",
+    nascimento: new Date("05/12/2013"),
+    materia: "Historia",
+  },
+  {
+    id: 10,
+    nome: "Lucas",
+    nascimento: new Date("04/03/2008"),
+    materia: "Filosofia",
+  },
+];
